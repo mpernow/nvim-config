f09e30f835cf5e11646268622e9a2ea86c8cd281
Marcus Pernow <marcus.pernow@gmail.com>
2025-01-28 20:19:22 +0100

    toggle terminal

--- lua/marcus/core/options.lua
+++ lua/marcus/core/options.lua
@@ -35,3 +35,33 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
+-- Term Toggle Function
+local term_buf = nil
+local term_win = nil
+
+function TermToggle(height)
+  if term_win and vim.api.nvim_win_is_valid(term_win) then
+    vim.cmd("hide")
+  else
+    vim.cmd("botright new")
+    local new_buf = vim.api.nvim_get_current_buf()
+    vim.cmd("resize " .. height)
+    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+      vim.cmd("buffer " .. term_buf) -- go to terminal buffer
+      vim.cmd("bd " .. new_buf) -- cleanup new buffer
+    else
+      vim.cmd("terminal")
+      term_buf = vim.api.nvim_get_current_buf()
+      vim.wo.number = false
+      vim.wo.relativenumber = false
+      vim.wo.signcolumn = "no"
+    end
+    vim.cmd("startinsert!")
+    term_win = vim.api.nvim_get_current_win()
+  end
+end
+
+-- Term Toggle Keymaps
+vim.keymap.set("n", "<C-t>", ":lua TermToggle(20)<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-t>", "<Esc>:lua TermToggle(20)<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:lua TermToggle(20)<CR>", { noremap = true, silent = true })
