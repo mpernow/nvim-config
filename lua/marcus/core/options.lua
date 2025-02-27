f45461af0750cfef2a303725b8b611e8df38c2c2
Marcus Pernow <marcus.pernow@gmail.com>
2025-02-27 20:08:04 +0100

    4 spaces for tabs

--- lua/marcus/core/options.lua
+++ lua/marcus/core/options.lua
@@ -8,8 +8,8 @@ opt.number = true
 opt.cursorline = true -- highlight current cursor line
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
